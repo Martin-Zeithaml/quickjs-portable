865009ec241f354d3733da7328636624a00cfda0
Martin Zeithaml <Martin.Zeithaml@broadcom.com>
2026-08-03 13:24:35 +0200

    z/OS update for malloc_usable_size
    Signed-off-by: Martin Zeithaml <Martin.Zeithaml@broadcom.com>

--- quickjs.c
+++ quickjs.c
@@ -2646,8 +2646,7 @@ static size_t js_def_malloc_usable_size(const void *ptr)
 #elif defined(__linux__)
     return malloc_usable_size((void *)ptr);
 #else
-    /* change this to `return 0;` if compilation fails */
-    return malloc_usable_size((void *)ptr);
+    return 0;
 #endif
 }
 
@@ -5132,7 +5131,7 @@ static JSValue JS_ConcatString1(JSContext *ctx,
     return JS_MKPTR(JS_TAG_STRING, p);
 }
 
-/* op1 and op2 are converted to strings. For convience, op1 or op2 =
+/* op1 and op2 are converted to strings. For convenience, op1 or op2 =
    JS_EXCEPTION are accepted and return JS_EXCEPTION.  */
 static JSValue JS_ConcatString(JSContext *ctx, JSValue op1, JSValue op2)
 {
